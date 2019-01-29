f67fba35b2539bef13dd1d0c803e53585cefc7d5
Gevorg Harutyunyan <gevharut@gmail.com>
2019-01-30 00:33:10 +0400

    Eslint fixes
    Tabulation size
    Classnames

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,8 +4,8 @@ import './Header.css';
 
 const Header = () => (
   <header className="Header">
-    <img src="/logo.png" alt="React Conference 2019 Logo" height=""></img>
-    <nav>      
+    <img className="Logo" src="/logo.png" alt="React Conference 2019 Logo" height="" />
+    <nav className="Navigation">
       <a href="#About">About</a>
       <a href="#Schedule">Schedule</a>
       <a href="#Speakers">Speakers</a>
@@ -14,7 +14,7 @@ const Header = () => (
       <a href="#Location">Location</a>
       <a href="#FAQ">FAQ</a>
       <a href="#Quiz">Quiz</a>
-      <button id="req-invite">Request an invite</button>      
+      <a href="#Invite" id="req-invite" className="Button">Request an invite</a>
     </nav>
   </header>
 );
