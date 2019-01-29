bd2bc031dfd7c7dbc7e8068e915811a8542474dc
alexigityan <40140063+alexigityan@users.noreply.github.com>
2019-01-29 14:24:47 +0400

    Add files via upload
    added top navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import Header from './Header/Header';
 import About from './About/About';
 
 import './App.css';
 
 const App = () => (
   <div className="App">
-    <About />
+    <Header />
   </div>
 );
 
---
+++ src/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Header.css';
+
+const Header = () => (
+  <header className="Header">
+    <img src="/logo.png" alt="React Conference 2019 Logo" height=""></img>
+    <nav>      
+      <a href="#">About</a>
+      <a href="#">Schedule</a>
+      <a href="#">Speakers</a>
+      <a href="#">Sponsors</a>
+      <a href="#">Team</a>
+      <a href="#">Location</a>
+      <a href="#">FAQ</a>
+      <a href="#">Quiz</a>
+      <a href="#" id="req-invite">Request an invite</a>      
+    </nav>
+  </header>
+);
+
+export default Header;
