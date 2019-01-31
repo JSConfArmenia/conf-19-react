35c50269d5e6663bff22ac403ac49448169d0c43
chris-manouckson <chris.manouckson@gmail.com>
2019-01-31 18:10:08 +0400

    Added About Section
    Contains heading and paragraph on the left side
    and polygon bordered image with aditional triangle and spark
    icons on the right.

--- src/About/About.js
+++ src/About/About.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
+import Title, { TitleAccent } from '../_components/Title';
+
 import './About.css';
 
 const About = () => (
   <div className="About">
-    <div className="Container">
-      <p>About Section</p>
+    <div className="info-container">
+      <Title>
+        What is <TitleAccent color="primary1">React Conf</TitleAccent>
+      </Title>
+      <p>
+        React Conf is an all-React conference,
+        gathering front-end and full-stack
+        developers in one place to share
+        knowledge, skills, to network, and
+        just to have fun.
+      </p>
     </div>
+    <img className="triangle" src="./about-section-triangle.png" alt="triangle" />
+    <img className="spark" src="./about-section-spark.png" alt="spark" />
+    <img className="main" src="./about-section-image.png" alt="about section" />
   </div>
 );
 
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from './Header/Header';
 import Intro from './Intro/Intro';
+import About from './About/About';
 
 import './root.scss';
 import './App.css';
@@ -10,6 +11,7 @@ const App = () => (
   <div className="App">
     <Header />
     <Intro />
+    <About />
   </div>
 );
 
