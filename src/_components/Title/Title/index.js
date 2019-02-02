3aa453f8cad21bf84c5c56781c7a24e0bc8c1f22
VamOSGS <gegham.samvelyan@hotmail.com>
2019-02-02 11:27:09 +0400

    Fixed fixed Title component

--- src/_components/Title/Title/index.js
+++ src/_components/Title/Title/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './styles.scss';
 
-const Title = ({ className, ...props }) => (
-  <h1 className={`title ${className}`} {...props} />
+const Title = ({ className, children, ...props }) => (
+  <h1 className={`title ${className}`} {...props}>
+    {children}
+  </h1>
 );
 
 Title.propTypes = {
