1c093de095f4e1938b71a544595eb2da447ce936
Gevorg Harutyunyan <gevharut@gmail.com>
2019-04-01 20:04:30 +0400

    Speakers section update
    Optimize Speaker image

--- src/Speakers/Speakers.js
+++ src/Speakers/Speakers.js
@@ -17,6 +17,7 @@ const speakersList = [
   'Elizabet_Oliveira',
   'Jesse_Martin',
   'Khachatur_Virabyan',
+  'Rebecca_Hill',
   'Eugene_Zharkov',
   'Varik_Matevosyan',
   'Leandro_Ostera',
@@ -24,7 +25,6 @@ const speakersList = [
   'Gagik_Arustamyan',
   'Radoslaw_Malecki',
   'Miłosz_Piechocki',
-  'Rebecca_Hill',
 ].map(id => ({
   id,
   ...speakersSerivce[id],
