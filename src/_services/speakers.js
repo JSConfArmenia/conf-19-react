e8c7d7eb759ad55ad491a854c3658824a0a0c27d
Varser Zakaryan <37110358+varszakaryan@users.noreply.github.com>
2019-04-07 02:11:32 +0400

    Update speakers.js

--- src/_services/speakers.js
+++ src/_services/speakers.js
@@ -6,7 +6,7 @@ export default {
     topic: 'Product Ready React with GatsbyJS',
     socialUrls: [
       'https://github.com/motleydev',
-      'https://twitter.com/motleydev',
+      'https://twitter.com/motleydev'
       ],
   },
   Roy_Derks: {
@@ -16,7 +16,7 @@ export default {
     topic: 'Handling Large-Scale State-Management with Context and Hooks',
     socialUrls: [
       'https://github.com/royderks',
-      'https://twitter.com/gethackteam',
+      'https://twitter.com/gethackteam'
       ],
   },
   Varik_Matevosyan: {
@@ -26,7 +26,7 @@ export default {
     topic: 'Testing React in a right way',
     socialUrls: [
       'https://github.com/var77',
-      'https://twitter.com/D4RK7ET',
+      'https://twitter.com/D4RK7ET'
       ],
   },
   Khachatur_Virabyan: {
@@ -36,7 +36,7 @@ export default {
     topic: 'Dynamic styling systems with React',
     socialUrls: [
       'https://github.com/Khachatour',
-      'https://twitter.com/kh_mugh',
+      'https://twitter.com/kh_mugh'
       ],
   },
   Eugene_Zharkov: {
@@ -46,7 +46,7 @@ export default {
     topic: 'Monorepo: React Web & React Native',
     socialUrls: [
       'https://github.com/2j2e',
-      'https://twitter.com/2j2e',
+      'https://twitter.com/2j2e'
       ],
   },
   Leandro_Ostera: {
@@ -56,7 +56,7 @@ export default {
     topic: 'A Reasonable Web',
     socialUrls: [
       'https://github.com/ostera',
-      'https://twitter.com/leostera',
+      'https://twitter.com/leostera'
       ],
   },
   Sara_Vieira: {
@@ -64,76 +64,106 @@ export default {
     name: 'Sara Vieira',
     position: 'Developer at CodeSandbox',
     topic: 'Let’s manage our local state with GraphQL',
-    socialUrls: [],
+    socialUrls: [
+      'https://github.com/SaraVieira',
+      'https://twitter.com/NikkitaFTW'
+      ],
   },
   Denys_Radin: {
     img: '/speakers/Denys_Radin.jpg',
     name: 'Denys Radin',
     position: 'Author of HTML-GL, ReactiveElements, JavaScript/WebGL engineer at Evolution Gaming',
     topic: 'Photorealistic 3D graphics on the Web with ReactJS, WebGL and Unity3D',
-    socialUrls: [],
+    socialUrls: [
+      'https://github.com/PixelsCommander',
+      'https://twitter.com/PixelsCommander'
+      ],
   },
   Gagik_Arustamyan: {
     img: '/speakers/Gagik_Arustamyan.jpg',
     name: 'Gagik Arustamyan',
     position: 'Head of Front-end/JavaScript development at SFL',
     topic: 'From design systems to component libraries',
-    socialUrls: [],
+    socialUrls: [
+      'https://github.com/arrustamyan'
+      ],
   },
   Radoslaw_Malecki: {
     img: '/speakers/Radoslaw_Malecki.jpg',
     name: 'Radoslaw Malecki',
     position: 'Senior Frontend Developer at OLX Group',
     topic: 'Let\'s make a photo booth in React!',
-    socialUrls: [],
+    socialUrls: [
+      'https://github.com/radosny',
+      'https://twitter.com/radoslawmalecki'
+      ],
   },
   Narendra_Shetty: {
     img: '/speakers/Narendra_Shetty.jpg',
     name: 'Narendra Shetty',
     position: 'Senior Frontend Developer at Twilio',
     topic: 'Turning an Application UI into an API',
-    socialUrls: [],
+    socialUrls: [
+      'https://github.com/narendrashetty',
+      'https://twitter.com/narendra_shetty'
+      ],
   },
   Miłosz_Piechocki: {
     img: '/speakers/Miłosz_Piechocki.jpg',
     name: 'Miłosz Piechocki',
     position: 'Senior Front-end Developer at SumoLogic',
     topic: 'Make your React code bulletproof with some TypeScript wizardry',
-    socialUrls: [],
+    socialUrls: [
+      'https://github.com/miloszpp',
+      'https://twitter.com/miloszpp'
+      ],
   },
   Cristian_Oliveira_da_Rosa: {
     img: '/speakers/Cristian_Oliveira_da_Rosa.jpg',
     name: 'Cristian Oliveira da Rosa',
     position: 'Software Developer at Sumup.com',
     topic: 'React to the need for speed',
-    socialUrls: [],
+    socialUrls: [
+      'https://github.com/cristianoliveira',
+      'https://twitter.com/cristian_or_'
+      ],
   },
   Elizabet_Oliveira: {
     img: '/speakers/Elizabet_Oliveira.jpg',
     name: 'Elizabet Oliveira',
     position: 'Senior UX Designer at Optum',
     topic: 'An SVG’s Tale',
-    socialUrls: [],
+    socialUrls: [
+      'https://github.com/miukimiu',
+      'https://twitter.com/miuki_miu'
+      ],
   },
   Lilit_Tadevosyan: {
     img: '/speakers/Lilit_Tadevosyan.jpg',
     name: 'Lilit Tadevosyan',
     position: 'Lead JavaScript developer at SFL',
     topic: 'N/A',
-    socialUrls: [],
+    socialUrls: [
+      'https://github.com/lilittad'
+      ],
   },
   Rebecca_Hill: {
     img: '/speakers/Rebecca_Hill.jpg',
     name: 'Rebecca Hill',
     position: 'Senior Frontend Developer and Team Lead at WeTransfer',
     topic: 'Structuring your app\'s business logic',
-    socialUrls: [],
+    socialUrls: [
+      'https://github.com/beerecca',
+      'https://twitter.com/rebekaka'
+      ],
   },
   Taguhi_Asatryan: {
     img: '/speakers/Taguhi_Asatryan.jpg',
     name: 'Taguhi Asatryan',
     position: 'Full-Stack Developer at Teamable',
     topic: 'Testing React Ecosystem: Do\'s and Don\'ts',
-    socialUrls: [],
+    socialUrls: [
+      'https://github.com/Taguhi',
+      ],
   },
 };
