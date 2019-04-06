8aa24575b510092332e8810fd8607e0db2391ece
Varser Zakaryan <37110358+varszakaryan@users.noreply.github.com>
2019-04-07 01:38:47 +0400

    Update JobList.js

--- src/Jobs/JobList.js
+++ src/Jobs/JobList.js
@@ -1,18 +1,18 @@
 export default [
   {
-    title: 'Senior IOS Developer',
-    company: 'Develandoo',
-    url: 'https://google.com',
+    title: 'Sr. Software Engineer, Front End',
+    company: 'ServiceTitan',
+    url: 'https://boards.greenhouse.io/servicetitan/jobs/1573586',
   },
   {
-    title: 'Android Developer',
-    company: 'Joomag',
-    url: 'https://google.com',
+    title: 'Senior Software Engineer',
+    company: 'ServiceTitan',
+    url: 'https://boards.greenhouse.io/servicetitan/jobs/1573606',
   },
   {
-    title: 'React JS Developer',
-    company: 'Simply Technologies',
-    url: 'https://google.com',
+    title: 'Software Engineer',
+    company: 'VMware',
+    url: 'https://careers.vmware.com/job/yerevan/software-engineer/1567/11255266',
   },
   {
     title: 'Node JS Developer',
