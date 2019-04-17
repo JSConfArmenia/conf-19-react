27c16b94f33156de480e068f61389a119585a215
Varser Zakaryan <37110358+varszakaryan@users.noreply.github.com>
2019-04-17 18:38:17 +0400

    Update JobList.js

--- src/Jobs/JobList.js
+++ src/Jobs/JobList.js
@@ -14,4 +14,19 @@ export default [
     company: 'VMware',
     url: 'https://careers.vmware.com/job/yerevan/software-engineer/1567/11255266',
   },
+  {
+    title: 'Engineering Manager',
+    company: 'Vineti AM',
+    url: 'https://recruiting.paylocity.com/Recruiting/Jobs/Details/110679',
+  },
+  {
+    title: 'Full-Stack Software Engineer',
+    company: 'Vineti AM',
+    url: 'https://recruiting.paylocity.com/Recruiting/Jobs/Details/59317',
+  },
+  {
+    title: 'Mid/Senior JavaScript Developer',
+    company: 'Digitain',
+    url: 'https://digitain.com/vacancy/mid-senior-javascript-developer-2/',
+  },
 ];
